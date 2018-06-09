a7e519fa33f4e964b349a0b915bafd8ed0cef194
Lydia Hallie <lydiajuliettehallie@gmail.com>
2018-06-09 22:30:37 +0200

    Update style loader

--- client/config/webpack.config.prod.js
+++ client/config/webpack.config.prod.js
@@ -214,7 +214,7 @@ module.exports = {
           },
           {
             test: /\.sass$/,
-            loaders: ['style', 'css', 'sass']
+            loaders: ['style-loader', 'css-loader', 'sass-loader']
           },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
