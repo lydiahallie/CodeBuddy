6ca6ce7eeb5b0f9e5ea5e1053c17c7820243215c
Lydia Hallie <lydiajuliettehallie@gmail.com>
2018-06-18 17:12:31 +0200

    Heroku

--- client/src/components/LandingPage/Footer/footerContent.js
+++ client/src/components/LandingPage/Footer/footerContent.js
@@ -1,18 +1,18 @@
 export const FOOTER_CONTENT = [
-    { 
-      name: 'About', 
-      links: [
-        { name: 'Blog', url: '/'},
-        { name: 'GitHub', url: '/'},
-        { name: 'Contact Us', url: '/'},
-      ]
-    },
-    { 
-      name: 'Social', 
-      links: [
-        { name: 'Twitter', url: '/'},
-        { name: 'Instagram', url: '/'},
-        { name: 'Slack', url: '/'},
-      ]
-    },
-  ];
+  { 
+    name: 'About', 
+    links: [
+      { name: 'Blog', url: '/'},
+      { name: 'GitHub', url: '/'},
+      { name: 'Contact Us', url: '/'},
+    ]
+  },
+  { 
+    name: 'Social', 
+    links: [
+      { name: 'Twitter', url: '/'},
+      { name: 'Instagram', url: '/'},
+      { name: 'Slack', url: '/'},
+    ]
+  },
+];
