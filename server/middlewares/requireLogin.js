b3d040a8ce6d2bd4de4c37d022d7a1aebaff29d8
Satyadeep Roat <satyadeep.roat@gmail.com>
2018-08-27 00:14:28 +0530

    login middleware update

--- server/middlewares/requireLogin.js
+++ server/middlewares/requireLogin.js
@@ -1,4 +1,6 @@
 module.exports = (req, res, next) => {
-    if (!req.user) res.status(401).send({ error: 'You must log in!' });
+    if (!req.user) {
+    	return res.status(401).send({ error: 'You must log in!' });
+    }
     next();
 };
