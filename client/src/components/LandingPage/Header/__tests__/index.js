a9ccf43fb78fdd7f8123e28e46920f9dcb0c0c5f
Ben Styles <enquiries@leeds-ebooks.co.uk>
2018-09-02 16:06:12 +0100

    Add first passing jest test

---
+++ client/src/components/LandingPage/Header/__tests__/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  render,
+  cleanup,
+} from 'react-testing-library';
+import { Header } from '..';
+
+describe('Header component', () => {
+  afterEach(cleanup);
+
+  it('renders correctly', () => {
+    expect(render(<Header />).container).toMatchSnapshot();
+  });
+});
