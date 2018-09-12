fa5cffc3b9b961e3b48dc1385fbfaa825283a81f
Satyadeep Roat <satyadeep.roat@gmail.com>
2018-09-12 23:20:31 +0530

    req.user set using a custom callback for authentication in passportjs

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -27,16 +27,22 @@ module.exports = app => {
         if (!user) {
           return res.status(400).send({ error: info });
         }
-        const {
-          profile, firstName, lastName, _id, email, __v,
-        } = user;
-        return res.send({
-          profile,
-          firstName,
-          lastName,
-          _id,
-          email,
-          __v,
+        
+        return req.logIn(user, error => {
+          if(error) {
+            return res.send(500).send({error});
+          }
+          const {
+            profile, firstName, lastName, _id, email, __v,
+          } = user;
+          return res.send({
+            profile,
+            firstName,
+            lastName,
+            _id,
+            email,
+            __v,
+          });
         });
       })(req, res, next);
     }
@@ -52,16 +58,21 @@ module.exports = app => {
         if (!user) {
           return res.status(400).send({ error: info });
         }
-        const {
-          profile, firstName, lastName, _id, email, __v,
-        } = user;
-        return res.send({
-          profile,
-          firstName,
-          lastName,
-          _id,
-          email,
-          __v,
+        return req.logIn(user, error => {
+          if(error) {
+            return res.send(500).send({error});
+          }
+          const {
+            profile, firstName, lastName, _id, email, __v,
+          } = user;
+          return res.send({
+            profile,
+            firstName,
+            lastName,
+            _id,
+            email,
+            __v,
+          });
         });
       })(req, res, next);
     }
