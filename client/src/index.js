e632d0a7f65da8dbc865581a867790d50c8f6351
Lydia Hallie <lydiajuliettehallie@gmail.com>
2018-09-15 19:16:26 +0200

    Connect React Apollo

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
@@ -17,15 +19,22 @@ const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store;
 
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+});
+
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route path="/dashboard/:name" component={Dashboard} />
-      </Switch>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/dashboard/:name" component={Dashboard} />
+        </Switch>
+      </Router>
+    </ApolloProvider>
   </Provider>,
   document.getElementById('root')
 );
+
 registerServiceWorker();
